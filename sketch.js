e6bedbe971253a23be77752bec63391cad7961ea
singamsricharan <singam.sricharan1603@gmail.com>
2020-11-06 16:17:19 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
-var ball;
+var hypnoticball;
 
 function setup(){
+    database=firebase.database();
+    console.log(database);
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+
+    hypnoticball = createSprite(250,250,10,10);
+    hypnoticball.shapeColor = "red";
+
+    var hypnoticBallposition =database.ref("ball/position");
+    hypnoticBallposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+    if(position!==unidefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+}
+function writePositon(x,y){
+database.ref("ball/positon").set({
+    'x':positon.x+x,
+    'y':positon.y+y
+})
+}
+function readPosition(data){
+    position=data.val();
+    hypnoticball.x = hypnoticball.x + x;
+    hypnoticball.y = hypnoticball.y + y;
 }
